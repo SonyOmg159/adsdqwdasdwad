cf2b4c598ebae7284e1c92623918a3141e3a1a15
SonyOmg159 <42983072+SonyOmg159@users.noreply.github.com>
2018-11-16 18:41:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,10 +17,7 @@ client.on('ready', () => {
   console.log('')
   console.log('╚[════════════════════════════════════]╝')
 });
-client.on('ready', () => {
-     client.user.setActivity("MineCraft|Gamers",{type: 'WATCHING'})
 
-});
  
 
 
@@ -31,13 +28,13 @@ const devs = ['200644160179535872' , '507260598082338837' , '' , ''];
               var argresult = message.content.split(` `).slice(1).join(' ');
                 if (!devs.includes(message.author.id)) return;
 
-            if (message.content.startsWith(adminprefix + 'st')) {
+            if (message.content.startsWith(adminprefix + '=')) {
               client.user.setGame(argresult, "https://www.twitch.tv/ Minecraft|Gamers");
                 message.channel.sendMessage(`**✅   ${argresult}**`)
             }
             });
 
-client.login(process.env.BOT_TOKEN);
+
 
 
 
